cdb8addb35d6df6a5adfea09c05f7e4e8df2461c
Nik Hixson <nikilobe@gmail.com>
2025-04-03 09:35:49 -0700

    Add test to check for visible products on the home page

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -2,4 +2,9 @@ describe("Home Page", () => {
   it("Visits the home page", () => {
     cy.visit("/");
   });
+
+  it("There are products on the page", () => {
+    cy.visit("/");
+    cy.get(".products article").should("be.visible");
+  });
 });
